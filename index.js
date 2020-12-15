d10cff3e8730cf508f512aa5d53610cede2216bf
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-12-15 16:37:54 -0500

    [index] Add blogpost route

--- index.js
+++ index.js
@@ -3,12 +3,41 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-const db = require('./models/blog.js');
+const BlogPost = require('./models/blog.js');
+
+// Connect to database
+mongoose.connect(`mongodb://localhost/mongooseAssociation`);
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log(`Connected to MongoDB on ${db.host}:${db.port}`);
+});
+
+db.on('error', (err) => {
+    console.log(`Error`, err);
+});
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('home route, backend');
+});
+
+app.get('/blog', (req, res) => {
+    // One way to create a post
+    BlogPost.create({ 
+        title: 'Mongoose for all Mongoose', 
+        body: 'This is a cool blog post.'
+    });
+    
+    // Another way to create blog post and save to database
+    const post1 = new BlogPost({
+        title: 'SEI 1019',
+        body: 'Software Engineers are cool.'
+    });
+    post1.save();
+    res.send('Post completed');
 });
 
 const PORT = process.env.PORT || 3000;
